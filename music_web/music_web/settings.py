87330d2f7e38e9313deb4a96502eb56119d5de65
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-04-29 14:32:13 +0530

    tmplates folder for authentiat

--- music_web/music_web/settings.py
+++ music_web/music_web/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'authenticat'
+    
+    'authenticat.apps.AuthenticatConfig'
 ]
 
 MIDDLEWARE = [
