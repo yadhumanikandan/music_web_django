7921e8279a3e2d782d22116d7fab339e0fadcf2c
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-04-30 14:48:54 +0530

    user authentication database done

--- music_web/authenticat/views.py
+++ music_web/authenticat/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        email = request.POST.get("email")
+
+        user = User.objects.create_user(username=username, password=password, email=email)  ## at this point the user is already created in the database
+
+        # user.last_name = "something"
+        # user.save()                                                            ##  not mandatory only used when updating some properties
+
+        return HttpResponse(username)
+
+    else:
+        return render(request, 'login.html')
