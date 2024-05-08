575f39ea3945c3f4279e0c880572ec182e5190a5
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-05-08 23:14:29 +0530

    admin dashboard and add scale page done

--- music_web/music_admin/forms.py
+++ music_web/music_admin/forms.py
@@ -7,3 +7,9 @@ class ScaleForm(forms.ModelForm):
     class Meta:
         model = Scale
         fields = ['name', 'scale', 'discription', 'keyboard_image', 'guitar_image']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'scale': forms.TextInput(attrs={'class': 'form-control'}),
+            'discription': forms.Textarea(attrs={'class': 'form-control'}),
+
+        }
