26d1b332d99e1f24091dc8abcd54d6ee8a87ed38
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-05-10 17:16:08 +0530

    added account page with logout function

--- music_web/base/urls.py
+++ music_web/base/urls.py
@@ -6,5 +6,6 @@ app_name = "base"
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('show-scales', views.show_scales, name='show_scales')
+    path('show-scales/', views.show_scales, name='show_scales'),
+    path('account/', views.account, name='account')
 ]
--- music_web/base/views.py
+++ music_web/base/views.py
@@ -9,6 +9,15 @@ def home(request):
     return render(request, 'base/home.html')
 
 
+
+def account(request):
+    if request.user.is_authenticated:
+        return render(request, "base/account.html")
+    
+    else:
+        return redirect('authenticat:signin')
+
+
 def show_scales(request):
     data = Scale.objects.all()
     return render(request, 'base/display_data.html', {'data': data})
