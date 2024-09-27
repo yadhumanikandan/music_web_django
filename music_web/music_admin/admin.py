083775c5817b254d4d03530b8b9562bf8b5b863c
yadhumanikandan <yadhumanikandan0@gmail.com>
2024-09-27 13:11:24 +0530

    chords model in admin panel

--- music_web/music_admin/admin.py
+++ music_web/music_admin/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Scale
+from .models import Scale, Chord
 
 # Register your models here.
 
 
 admin.site.register(Scale)
+admin.site.register(Chord)
